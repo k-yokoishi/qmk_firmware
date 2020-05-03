3f154afcf0dd11e1f46fddf2a234a31e7d2f8bf4
k-yokoishi <ss.xs.m713@gmail.com>
2020-05-03 18:11:06 +0900

    Replace bracket and curly bracee position

--- keyboards/ergodash/rev1/keymaps/k-yokoishi/keymap.c
+++ keyboards/ergodash/rev1/keymaps/k-yokoishi/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * | Esc  |   1  |   2  |   3  |   4  |   5  |  `   |                    |   =  |   6  |   7  |   8  |   9  |   0  |  -   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |                    |   ]  |   Y  |   U  |   {  |   }  |   P  |  \   |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |                    |   ]  |   Y  |   U  |   [  |   ] |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  | NONE |                    |   /  |   H  |   =  |   [  |   ]  |   ;  |  '   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  | NONE |                    |   /  |   H  |   =  |   {  |   }  |   ;  |  '   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Space|||||||| Alt  |      |   N  |   M  |   <  |   >  |  Up  | Shift|
    * |-------------+------+------+------+------| Space|------+------+------| Enter|------+------+------+------+-------------|
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT_4key_2u_inner(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                         KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_LCBR, KC_RCBR, KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,                        KC_SLSH, KC_H,    KC_EQL,  KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_LBRC, KC_RBRC, KC_P,    KC_BSLS, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,                        KC_SLSH, KC_H,    KC_EQL,  KC_LCBR, KC_RCBR, KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,                         KC_LALT, KC_N,    KC_M,    KC_LABK, KC_RABK, KC_UP,   KC_RSFT, \
     KC_ESC,  KC_LALT, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_BSPC,                        KC_BSPC, KC_ENT,  RAISE,   KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
