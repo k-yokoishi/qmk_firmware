78b9558968c2a6146d3fde8f9fd355a18b149a99
k-yokoishi <ss.xs.m713@gmail.com>
2020-05-26 20:47:44 +0900

    Set number keymaps to raise layer

--- keyboards/ergodash/rev1/keymaps/k-yokoishi/keymap.c
+++ keyboards/ergodash/rev1/keymaps/k-yokoishi/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  | NONE |                    |   /  |   +  |   =  |   {  |   }  |   :  |  '   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Space|||||||| Alt  |      |   N  |   M  |   <  |   >  |  Up  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Space|||||||| Alt  |      |   |  |   -  |   <  |   >  |  Up  | Shift|
    * |-------------+------+------+------+------| Space|------+------+------| Enter|------+------+------+------+-------------|
    * | Cmd  |  Alt |  Alt | Cmd  ||||||||LW/EIS|      |  Del |||||||| Bspc |      |RS/KAN|||||||| Cmd  | Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_GRV,                         KC_EQL,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_TILD, KC_GRV,  KC_LBRC, KC_RBRC, KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,                        KC_SLSH, KC_PPLS, KC_EQL,  KC_LCBR, KC_RCBR, KC_COLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,                         KC_LALT, KC_N,    KC_M,    KC_LABK, KC_RABK, KC_UP,   KC_RSFT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,                         KC_LALT, KC_UNDS, KC_MINS, KC_LABK, KC_RABK, KC_UP,   KC_RSFT, \
     KC_ESC,  KC_LALT, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_BSPC,                        KC_BSPC, KC_ENT,  RAISE,   KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * | Esc  |   1  |   2  |   3  |   4  |   5  |  `   |                    |   =  |   6  |   7  |   8  | Mute | Vol- | Vol+ |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * | Tab  |   1  |   2  |   3  |   4  |   5  |  [   |                    |   ]  |   6  |   7  |   8  |   9  |   0  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  | NONE |                    |   B  | Left | Down |  Up  | Right|   ;  |  '   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_RAISE] = LAYOUT_4key_2u_inner(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                         KC_EQL,  KC_6,    KC_7,    KC_8,    KC__MUTE, KC__VOLDOWN, KC__VOLUP, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,                        KC_B,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,                         KC_LALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP, \
     KC_ESC,  KC_LALT, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_BSPC,                        KC_BSPC, KC_ENT,  RAISE,   KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
