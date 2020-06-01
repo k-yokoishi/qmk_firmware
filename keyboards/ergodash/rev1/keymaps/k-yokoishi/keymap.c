ce05ccede173fa5b7f075621ce78db33496e7903
k-yokoishi <ss.xs.m713@gmail.com>
2020-06-01 22:17:52 +0900

    Set Space and Shift

--- keyboards/ergodash/rev1/keymaps/k-yokoishi/keymap.c
+++ keyboards/ergodash/rev1/keymaps/k-yokoishi/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Ctrl |   A  |   S  |   D  |   F  |   G  | NONE |                    |   B  |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Space|||||||| Alt  |      |   N  |   M  |   ,  |   .  |   /  | Up   |
-   * |-------------+------+------+------+------| Space|------+------+------| Enter|------+------+------+------+-------------|
+   * |-------------+------+------+------+------| SandS|------+------+------| Enter|------+------+------+------+-------------|
    * | Cmd  |  Alt |  Alt | Cmd  ||||||||LW/EIS|      | Shift|||||||| Bspc |      |RS/KAN|||||||| Cmd  | Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,                        KC_B,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,                         KC_LALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP, \
-    KC_ESC,  KC_LALT, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_LSFT,                       KC_BSPC, KC_ENT,  RAISE,   KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_ESC,  KC_LALT, KC_LALT, KC_LGUI, LOWER,   SFT_T(KC_SPC),  KC_LSFT,                       KC_BSPC, KC_ENT,  RAISE,   KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   /* Lower
